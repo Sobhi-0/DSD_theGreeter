2d687f11a829fb9734137afe8612260473847b02
Jonas Schmidt <jon-schmidt@outlook.de>
2024-10-26 16:18:30 +0200

    update to homeassistant ros node

--- ros_ws/src/homeassitant/homeassitant/homeassitant_node.py
+++ ros_ws/src/homeassitant/homeassitant/homeassitant_node.py
@@ -1,6 +1,15 @@
+from rclpy.node import Node
+
+
+class HomeAssistantNode(Node):
+    def __init__(self):
+        super().__init__("homeassistant")
+        self.get_logger().info("HomeAssistantNode has been started.")
+
+
 def main():
-    print('Hi from homeassitant.')
+    print("Hi from homeassitant.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
